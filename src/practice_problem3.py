6e4112b32b5f1aebeaaec03b7f2c2f16c5a3841f
scottjm1 <jasminescott2018@gmail.com>
2018-10-07 22:12:09 -0400

    Practice Problem #3

--- src/practice_problem3.py
+++ src/practice_problem3.py
@@ -551,11 +551,12 @@ def practice_problem3e(sequence):
 
     total = 0
     for k in range(len(sequence)):
-        total = total + sequence[k * 2]
+        if k % 2 == 0:
+            total = total + sequence[k]
     return total
 
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # COMPLETED: 7. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
